9a45fb0c60402010638a5828c16da7421510a04c
Marco Antônio <mfontalvoantunes@gmail.com>
2023-06-02 12:59:19 -0300

    generateAnswerParts and send

--- commands/gpt/gpt4.cjs
+++ commands/gpt/gpt4.cjs
@@ -6,22 +6,62 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gpt')
 		.setDescription('Ask anything!')
-		.addStringOption(option => option.setName('question')
-			.setDescription('Enter your question')
+		.addStringOption(option => option.setName('prompt')
+			.setDescription('Enter your prompt here.')
 			.setRequired(true)),
 
 	async execute(interaction) {
-		await interaction.reply({ content: 'Thinking...', ephemeral: false });
-
 		try {
+			await interaction.deferReply();
+
 			const response = await axios.get(`${openaiUrl}/discord`, {
-				params: { prompt: interaction.options.getString('question'), }
+				params: { prompt: interaction.options.getString('prompt'), }
 			});
 
-			interaction.editReply({ content: `${response.data}` });
+			const answerParts = await this.generateAnswerParts(response.data);
+
+			answerParts.forEach(async element => {
+				interaction.channel.send(element);
+			});
+
+			await interaction.editReply('Done!');
 		}
 		catch (error) {
-			interaction.editReply({ content: `An exception has ocurred.` });
+			interaction.editReply({ content: `An exception has ocurred. ${error}` });
 		}
 	},
+
+	async generateAnswerParts(answer) {
+		// Separate code snippets from the answer using regex
+		const codeSnippetPattern = /```[\s\S]*?```/g;
+		const codeSnippets = Array.from(answer.matchAll(codeSnippetPattern)).map(m => m[0]);
+		const nonCodeParts = answer.split(codeSnippetPattern);
+
+		// Combine non-code parts and code snippets into a new list
+		let answerParts = [];
+		let snippetCount = codeSnippets.length;
+		for (let i = 0; i < nonCodeParts.length; i++) {
+			const nonCodePart = nonCodeParts[i].trim();
+			const codeSnippet = i < snippetCount ? codeSnippets[i].trim() : '';
+
+			if (nonCodePart) {
+				answerParts.push(nonCodePart);
+			}
+			if (codeSnippet) {
+				answerParts.push(codeSnippet);
+			}
+		}
+
+		// Split the answer parts if they exceed the character limit
+		const maxLength = 2000;
+		let splitAnswerParts = [];
+		for (const part of answerParts) {
+			for (let i = 0; i < part.length; i += maxLength) {
+				const blockSize = Math.min(maxLength, part.length - i);
+				splitAnswerParts.push(part.substring(i, i + blockSize));
+			}
+		}
+
+		return splitAnswerParts;
+	}
 };  
